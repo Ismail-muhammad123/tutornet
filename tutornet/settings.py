e6f554747a5833e4f1e923cf3fd114afb00f979e
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2024-05-19 17:15:36 +0100

    added sql

--- tutornet/settings.py
+++ tutornet/settings.py
@@ -122,10 +122,10 @@ MESSAGE_TAGS = {
 
 
 if not DEBUG:
-    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME","")
+    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME","")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID","")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY","")
     # Tell django-storages the domain to use to refer to static files.
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
